b7e9c88274a33c79938f918c530853f17c721c0a
Kaiden Flaig <kaidenflaig335@gmail.com>
2023-01-21 15:35:51 -0700

    fixed styling, added timeline, formatted code, etc. etc.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 //variables
 let addCalorieBtn = document.querySelector("#add-calories-btn")
-let removeEntryBtn = document.querySelector("#remove-entry-btn")
+let removeEntryBtn = document.querySelector("#remove-day-entry-btn")
+let removeDayBtn = document.querySelector("#remove-timeline-entry-btn")
 let clearBtn = document.querySelector("#clear-btn")
-let caloriesListContainer = document.querySelector(".entries-list")
+let caloriesListContainerDay = document.querySelector("#day-list")
+let caloriesListContainerTimeline = document.querySelector("#timeline-list")
 let calorieCounter = document.querySelector("#calorie-count")
+let submitDayBtn = document.querySelector("#submit-day-btn")
 let entriesCount = 1
+let entriesCountTimeline = 1
 let totalCalories = 0
 let caloriesList = []
+let caloriesTimeline = []
 
 //event listeners
 window.addEventListener("load", () => {
-    if (localStorage.getItem("caloriesList") != null && localStorage.getItem("entriesCount") != null){
+    if (localStorage.getItem("caloriesList") != null && localStorage.getItem("entriesCount") != null && localStorage.getItem("caloriesTimeline") != null){
         loadData()
         updateAllDisplays()
     }
@@ -29,16 +34,13 @@ addCalorieBtn.addEventListener("click", () => {
 })
 
 removeEntryBtn.addEventListener("click", () => {
-    let index = getValidNumber("Please enter the item number from the list", "must be a number") - 1
-    if (caloriesList.length > 0 && index >= 0){
-        while (index + 1 > caloriesList.length){
-            index = getValidNumber("Must be a valid list item", "must be a number") - 1
-        }
-        if (index >= 0 && index <= caloriesList.length - 1){
-            caloriesList.splice(index, 1)
-            updateAllDisplaysAndSave()
-        }
-    }
+    removeEntry(caloriesList)
+    updateAllDisplaysAndSave()
+})
+
+removeDayBtn.addEventListener("click", () => {
+    removeEntry(caloriesTimeline)
+    updateAllDisplaysAndSave()
 })
 
 clearBtn.addEventListener("click", () => {
@@ -46,24 +48,29 @@ clearBtn.addEventListener("click", () => {
     updateAllDisplaysAndSave()
 })
 
+submitDayBtn.addEventListener("click", () => {
+    caloriesTimeline.push(totalCalories)
+    totalCalories = 0
+    caloriesList = []
+    updateAllDisplaysAndSave()
+})
+
 
 //functions
-const updateListDisplay = () => {
-    caloriesListContainer.innerHTML = ""
+const updateListDisplay = (display, list) => {
+    display.innerHTML = ""
     entriesCount = 1
-    caloriesList.forEach(entry => {
-        caloriesListContainer.innerHTML += `${entriesCount}. ${entry}<br>`
+    list.forEach(entry => {
+        display.innerHTML += `${entriesCount}. ${entry}<br>`
         entriesCount ++
     })
 }
 
 const updateCalorieCounter = () => {
     totalCalories = 0
-
     caloriesList.forEach(entry => {
         totalCalories += parseInt(entry)
     })
-
     calorieCounter.innerText = totalCalories
 }
 
@@ -78,23 +85,39 @@ const getValidNumber = (promptText, errorText) => {
     return number
 }
 
+const removeEntry = (list) => {
+    let index = getValidNumber("Please enter the item number from the list", "must be a number") - 1
+    if (list.length > 0 && index >= 0){
+        while (index + 1 > list.length){
+            index = getValidNumber("Must be a valid list item", "must be a number") - 1
+        }
+        if (index >= 0 && index <= list.length - 1){
+            list.splice(index, 1)
+        }
+    }
+}
+
 const saveData = () => {
     localStorage.setItem("caloriesList", JSON.stringify(caloriesList))
     localStorage.setItem("entriesCount", JSON.stringify(entriesCount))
+    localStorage.setItem("caloriesTimeline", JSON.stringify(caloriesTimeline))
 }
 
 const loadData = () => {
     caloriesList = JSON.parse(localStorage.getItem("caloriesList"))
     entriesCount = JSON.parse(localStorage.getItem("entriesCount"))
+    caloriesTimeline = JSON.parse(localStorage.getItem("caloriesTimeline"))
 }
 
 const updateAllDisplaysAndSave = () => {
     updateCalorieCounter()
-    updateListDisplay()
+    updateListDisplay(caloriesListContainerDay, caloriesList)
+    updateListDisplay(caloriesListContainerTimeline, caloriesTimeline)
     saveData()
 }
 
 const updateAllDisplays = () => {
     updateCalorieCounter()
-    updateListDisplay()
+    updateListDisplay(caloriesListContainerDay, caloriesList)
+    updateListDisplay(caloriesListContainerTimeline, caloriesTimeline)
 }
